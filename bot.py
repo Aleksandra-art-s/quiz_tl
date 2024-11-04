f08d0873c6075f21832d8adce9f95dd28a664a7b
Aleksandra-art-s <412287@edu.itmo.ru>
2024-11-05 00:02:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,6 @@ from config import API_TOKEN
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 # Инициализируем бота и диспетчер
-bot = Bot(token=API_TOKEN)
+bot = Bot(token=7645679841:AAF9Kj-r8PIyFM_vVooVDlrLMF28p4Faf9g)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
