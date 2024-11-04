aead4dd7fc8089b76d759a6a3a66c329656b0cd0
Aleksandra-art-s <412287@edu.itmo.ru>
2024-11-04 23:47:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
 # app.py
 
 from aiogram import executor
-from bot import dp, on_startup
+from bot import dp
+from handlers import register_handlers  # импортируем функцию регистрации обработчиков
+from database import init_db  # импортируем инициализацию базы данных
 
-if __name__ == '__main__':
+async def on_startup(_):
+    await init_db()
+    print("Бот запущен и готов к работе.")
+
+# Регистрируем обработчики
+register_handlers(dp)
+
+# Запускаем бота
+if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
