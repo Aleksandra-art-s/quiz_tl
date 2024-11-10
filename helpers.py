c56c024e760a801a5795e5206bdea38f8a5c12fc
Aleksandra-art-s <412287@edu.itmo.ru>
2024-11-10 22:21:27 +0300

    Update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,11 +1,18 @@
 # helpers.py
 
 from database import async_session, Admin
+import logging
 
 async def is_admin(username):
+    if not username:
+        logging.info("Пользователь без юзернейма.")
+        return False
+    normalized_username = username.lstrip('@').lower()
     async with async_session() as session:
         result = await session.execute(
-            Admin.__table__.select().where(Admin.username == username)
+            Admin.__table__.select().where(Admin.username == normalized_username)
         )
         admin = result.fetchone()
-        return admin is not None
+        is_admin = admin is not None
+        logging.info(f"Проверка администратора '{normalized_username}': {is_admin}")
+        return is_admin
