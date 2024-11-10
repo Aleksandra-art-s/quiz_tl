03d33ecf698b10c95b73b0f337d33a42858a23bc
Aleksandra-art-s <412287@edu.itmo.ru>
2024-11-10 21:38:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # config.py
 
-API_TOKEN = '7645679841:AAF9Kj-r8PIyFM_vVooVDlrLMF28p4Faf9g'
+API_TOKEN = '7645679841:AAF9Kj-r8PIyFM_vVooVDlrLMF28p4Faf9g'  # Замените на ваш реальный токен
 
-# Список юзернеймов администраторов (без @)
-ADMIN_USERNAMES = ['MaierrA']
+# Путь к базе данных
+DATABASE_URL = 'sqlite+aiosqlite:///data/quiz.db'
